e5050b063a41150566d8af4dd44f11919d963915
lixiaofei002 <lixiaofei002@lianjia.com>
2018-04-10 23:15:49 +0800

    增加-[StringValueResolver接口注释]

--- spring-core/src/main/java/org/springframework/util/StringValueResolver.java
+++ spring-core/src/main/java/org/springframework/util/StringValueResolver.java
@@ -20,6 +20,7 @@ import org.springframework.lang.Nullable;
 
 /**
  * Simple strategy interface for resolving a String value.
+ * 用于解析字符串值的简单策略接口。
  * Used by {@link org.springframework.beans.factory.config.ConfigurableBeanFactory}.
  *
  * @author Juergen Hoeller
@@ -33,11 +34,16 @@ public interface StringValueResolver {
 
 	/**
 	 * Resolve the given String value, for example parsing placeholders.
-	 * @param strVal the original String value (never {@code null})
+	 * 解析给定的字符串值，例如解析占位符。
+	 * Nullable 表示定义的字段可以为空.
+	 * @param strVal the original String value (never {@code null}) 原始的字符串(不为null)
 	 * @return the resolved String value (may be {@code null} when resolved to a null
 	 * value), possibly the original String value itself (in case of no placeholders
 	 * to resolve or when ignoring unresolvable placeholders)
+	 * 已解析的字符串值（解析为空值时可能为空），
+	 * 可能是原始字符串值本身（如果没有占位符来解析或忽略无法解析的占位符）
 	 * @throws IllegalArgumentException in case of an unresolvable String value
+	 * 在无法解析字符串值的情况下 抛出IllegalArgumentException异常
 	 */
 	@Nullable
 	String resolveStringValue(String strVal);
